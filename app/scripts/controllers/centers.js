3461259df046dbddb2ae08af4f29270891ff5661
Dewey Hong <doohee323@gmail.com>
2013-10-11 10:48:57 +0900

    center grid finished

--- app/scripts/controllers/centers.js
+++ app/scripts/controllers/centers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('sheepgridApp')
-  .controller('CentersCtrl', function ($scope, $location, $routeParams, config, CenterService) {
+  .controller('CentersCtrl', function ($scope, $location, $routeParams, $timeout, config, CenterService) {
 	$scope.$location = $location;
 
     $scope.updateEntity = function(column, row, cellValue) {
@@ -47,28 +47,48 @@ angular.module('sheepgridApp')
 
     function getCenters() {
         CenterService.get({}, function(data) {
+            for (var i = 0; i < data.uip_centers.length; i++) {
+                data.uip_centers[i].status = 'R';
+            };
             $scope.uip_centers = data.uip_centers;
         });
     };
 
     getCenters();
 
+    $scope.$on('ngGridEventData', function (e,s) {
+        if($scope.gridCenter.selectRow) {
+            $scope.gridCenter.selectRow(0, true);
+            $(".ngViewport").focus();
+        }
+    });    
+
     $scope.getCenter = function () {
         getCenters();
     };
 
     $scope.insertCenter = function () {
-        var newData = getInitObj($scope.uip_centers[0]);
+        var data = $scope.gridCenter.columnDefs;
+        var newData = getAddRow(data);
         newData.status = 'I';
         $scope.uip_centers.unshift(newData);
+
+        var selectRow = function() {
+            $scope.gridCenter.selectRow(0, true);
+            //$($($(".ngCellText.col3.colt1")[1]).parent()).parent().focus();
+        }
+        $timeout(selectRow, 500);
     };
 
     $scope.deleteCenter = function () {
         var id = $scope.gridCenter.selectedItems[0].id;
         for (var i = 0; i < $scope.uip_centers.length; i++) {
             if($scope.uip_centers[i].id == id) {
-                $scope.uip_centers[i].status = 'D';
-                break;
+                if($scope.uip_centers[i].status == 'I') {
+                    $scope.uip_centers.splice(i, 1);
+                } else {
+                    $scope.uip_centers[i].status = 'D';
+                }
             }
         };
     };
@@ -85,36 +105,31 @@ angular.module('sheepgridApp')
         };
     };
 
-    $scope.saveCenter = function (center) {
-        debugger
-        for (var i = 0; i < center.length; i++) {
-            if(center[i].status) {
-                if(center[i].status == 'I') {
-                    var params = {uip_center : center[i]};
-                    if(config.server == 'spring') params = center[i]; // java
-                    CenterService.save(params, function (data) {
-                        $scope.uip_centers.unshift(data.uip_center);
-                        console.log(data);
-                    })
-                } else if(center[i].status == 'U') {
-                    var params = {uip_center : center[i],
-                                 id : center[i].id};
-                    if(config.server == 'spring') params = params.uip_center; // java
-                    CenterService.update(params, function (data) {
-                        console.log(data);
-                        lookupDs(center.id, function (row){
-                            $scope.uip_centers[row] = center;
-                        });
-                    })
-                } else if(center[i].status == 'D') {
-                    CenterService.delete({"id" : center.id}, function (data) {
-                        console.log(data);
-                        lookupDs(center.id, function (row){
-                            $scope.uip_centers.splice(row, 1);
-                        });
-                    })
-                }
+    $scope.saveCenter = function () {
+        var dataset = $scope.uip_centers;
+        for (var i = 0; i < dataset.length; i++) {
+            var status = dataset[i].status;
+            var currow = i;
+            delete dataset[i].status;
+            if(status == 'I') {
+                var params = {uip_center : dataset[i]};
+                if(config.server == 'spring') params = dataset[i]; // java
+                CenterService.save(params, function (data) {
+                    $scope.uip_centers[0].id = data.uip_center.id;
+                })
+            } else if(status == 'U') {
+                var params = {uip_center : dataset[i],
+                             id : dataset[i].id};
+                if(config.server == 'spring') params = params.uip_center; // java
+                CenterService.update(params, function (data) {
+                    $scope.uip_centers[currow] = data.uip_center;
+                })
+            } else if(status == 'D') {
+                CenterService.delete({"id" : dataset[i].id}, function (data) {
+                    $scope.uip_centers.splice(currow, 1);
+                })
             }
+            $scope.uip_centers[i].status = 'R';
         };
     };
 
@@ -127,12 +142,24 @@ angular.module('sheepgridApp')
 		}
 	}
 
-    var getInitObj = function(source) {
+    var getAddRow = function(source) {
         var data = angular.copy(source);
-        for (var i = 0; i < Object.keys(data).length; i++) {
-            data[Object.keys(data)[i]] = null;
+        var target = {};
+        for (var i = 0; i < data.length; i++) {
+            if(data[i].field && data[i].field != 'CHK') {
+                target[data[i].field] = null;
+            }
         };
-        return data;
+        return target;
     };
+
+    // var getInitObj = function(source) {
+    //     var data = angular.copy(source);
+    //     debugger
+    //     for (var i = 0; i < Object.keys(data).length; i++) {
+    //         data[Object.keys(data)[i]] = null;
+    //     };
+    //     return data;
+    // };
 
   });
